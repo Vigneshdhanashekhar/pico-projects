df68a924e4f33a8752f1e45709da7d99b64003fb
Vigneshdhanashekhar <vignesh.dhanashekhar.02@gmail.com>
2025-10-04 18:02:08 +0000

    Commiting for UART Rework

--- My_Projects/02_Pico_UART/uart_comm.c
+++ My_Projects/02_Pico_UART/uart_comm.c
@@ -15,7 +15,7 @@ int main() {
         gpio_put(LED_PIN, 1);
         printf("LED ON - Count: %d\n", count++);
         sleep_ms(1000);
-        
+
         gpio_put(LED_PIN, 0);
         printf("LED OFF\n");
         sleep_ms(1000);
